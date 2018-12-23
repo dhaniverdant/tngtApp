2177acb5300e42b427063d27d656afe6f01fd319
Ramdhani <dhaniverdant23@gmail.com>
2018-12-24 01:16:03 +0700

    adding screens & packages
    5 screens added, 2 packages installed

---
+++ src/screens/Booking/Booking.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Booking extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    Booking
+                </Text>
+            </View>
+        );
+    }
+}
