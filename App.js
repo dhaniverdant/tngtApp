2a0fc10cd1eb2adfdf7803e36bb75c1ff4179798
Ramdhani <dhaniverdant23@gmail.com>
2018-12-27 03:14:24 +0700

    Navigator in App

--- App.js
+++ App.js
@@ -1,13 +1,71 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import Home from './src/screens/Home/Home';
+import React from 'react';
+import { Dimensions, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
-export default class App extends Component {
+import Home from './src/screens/Home/Home';
+import AboutTngt from './src/screens/AboutTngt/AboutTngt';
+import Booking from './src/screens/Booking/Booking';
+import Gallery from './src/screens/Gallery/Gallery';
+import Profile from './src/screens/Profile/Profile';
+
+let screen = Dimensions.get('window');
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Home/>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Home />
       </View>
     );
   }
 }
+
+class AboutTngtScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AboutTngt />
+      </View>
+      );
+    }
+  }
+  
+class BookingScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Booking />
+      </View>
+    );
+  }
+}
+
+class GalleryScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Gallery />
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Profile />
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home: { screen: HomeScreen },
+  About: { screen: AboutTngtScreen },
+  Booking: { screen: BookingScreen },
+  Gallery: { screen: GalleryScreen },
+  Profile: { screen: ProfileScreen }
+});
+
+export default createAppContainer(TabNavigator);
