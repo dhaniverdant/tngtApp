24e04eaa9b4ecc5dda8e0a91ac9afbd3eb7f0955
Ramdhani <dhaniverdant23@gmail.com>
2019-01-16 00:32:10 +0700

    tidy up, re-styling and moving styles into different file

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,41 +1,23 @@
 import React, { Component } from 'react';
 import {
-    View,
-    Image,
-    StyleSheet,
-    Text
+  View,
+  Image
 } from 'react-native';
+import { estyles } from './style/HomeStyle'
 
 export default class Home extends Component {
-    render(){
-      const welcomeText = 'W E L C O M E !'
-      
-        return(
-            <View>
-                <Image
-                    source={require('../../assets/images/gunung_tambora.jpg')}
-                    style={ styles.imgBackground }
-                >
-                  <Text style={styles.textStyle}>{welcomeText}</Text>
-                </Image>
-            </View>
-        );
-    }
-}
+  render(){
+    const welcomeText = 'W E L C O M E !'
+    const mainImage = require('../../assets/images/gunung_tambora.jpg')
 
-const styles = StyleSheet.create ({
-    textStyle: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    imgBackground: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1
-    }
-})
+    return(
+      <View>
+        <Image
+          source={mainImage}
+          style={ estyles.imgBackground }
+        >
+        </Image>
+      </View>
+    )
+  }
+}
