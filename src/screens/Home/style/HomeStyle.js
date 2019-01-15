09f75a22cc6fea204b720dfc40304efa606d3cf5
Ramdhani <dhaniverdant23@gmail.com>
2019-01-16 00:31:25 +0700

    separating home styles into different file

---
+++ src/screens/Home/style/HomeStyle.js
@@ -0,0 +1,9 @@
+import EStyleSheet from 'react-native-extended-stylesheet'
+
+export const estyles = EStyleSheet.create ({
+  imgBackground: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  }
+})
