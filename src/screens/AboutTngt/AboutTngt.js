b072470a71dbd4593a0f716837f86f62b143a90e
Ramdhani <dhaniverdant23@gmail.com>
2019-01-22 18:48:55 +0700

    add swipeout

--- src/screens/AboutTngt/AboutTngt.js
+++ src/screens/AboutTngt/AboutTngt.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+import {Card} from 'react-native-elements'
+import Swipeout from 'react-native-swipeout'
 
 export default class AboutTngt extends Component {
-    render(){
-        return(
-            <View>
-                <Text>
-                    AboutTngt
-                </Text>
-            </View>
-        );
-    }
+
+  render() {
+  	var swipeoutBtns = [
+		  {
+		    text: 'Button'
+		  }
+		]
+    return (
+      <Swipeout right={swipeoutBtns} left={swipeoutBtns} >
+			  <Card style={{ height: 50, width: 200 }}>
+			    <Text>Swipe me left</Text>
+			  </Card>
+			</Swipeout>
+    )
+  }
 }
